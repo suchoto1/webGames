ad3c851ec23c76bb16a56fdaec6ea929e77b0026
Matěj Grohmann <grohmann@mobilesoft.eu>
2021-05-09 14:14:00 +0200

    project setup

---
+++ src/test/java/cz/uhk/pro3/draughts/WebDraughtsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.pro3.draughts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDraughtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
